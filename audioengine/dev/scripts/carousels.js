283b18b8b64dda321616bcbe297f957dc1d43ff9
nikia123 <nikia@nshaw.dev>
2023-03-31 12:04:39 -0700

    completed hero slider

---
+++ audioengine/dev/scripts/carousels.js
@@ -0,0 +1,15 @@
+//carousel script
+
+//hero carousel
+
+$(document).ready(function(){
+
+    $('.hero-slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true
+    })
+})
