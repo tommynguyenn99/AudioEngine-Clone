c98b597f70c31ee6e480ed4f6dd861c5ebc597a3
Tommy Nguyen <tommy.tuannguyen@outlook.com>
2023-03-31 13:21:18 -0700

    navbar fixed

--- audioengine/dev/scripts/script.js
+++ audioengine/dev/scripts/script.js
@@ -1,13 +1,13 @@
 // Hamburger Menu
 const hamburger = document.querySelector (".hamburger"); 
-const navMenu = document.querySelector(".nav-menu"); 
+const navMenu = document.querySelector(".nav-menu-mobile"); 
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active"); 
 })
 
-document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-links-mobile").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
