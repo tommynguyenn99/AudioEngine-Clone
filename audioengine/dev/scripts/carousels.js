3ee075303e2d86b99eb4b75d98367add2d7d15ca
nikia123 <nikia@nshaw.dev>
2023-04-01 15:35:23 -0700

    completed shop-by carousel and css

--- audioengine/dev/scripts/carousels.js
+++ audioengine/dev/scripts/carousels.js
@@ -1,3 +1,5 @@
+let $shopBy = $('.shop-by-container');
+
 //carousel script
 
 //hero carousel
@@ -13,3 +15,51 @@ $(document).ready(function(){
         fade: true
     })
 })
+
+
+
+//shop by carousel
+$(document).ready(function(){
+
+    function updateSlick(event) {
+        if (event.matches) { // We want Slick to run!
+          $('.shop-by-container').slick ({
+              dots: false,
+              arrows: true,
+              infinite: true,
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              responsive: [
+                  {
+                      breakpoint: 800,
+                      settings: {
+                          slidesToShow: 4,
+                          slidesToScroll: 4,
+                      }
+                  },
+                  {
+                      breakpoint: 1200,
+                      settings: {
+                          slidesToShow: 5,
+                          slidesToScroll: 5,
+                      }
+                  }
+              ]
+          })
+        } else { // 'unslick'
+            $('.shop-by-container').slick('unslick')
+        }
+      }
+      
+      const mediaQuery = window.matchMedia("(min-width: 432px)")
+      updateSlick(mediaQuery) // Call listener function at run time
+      mediaQuery.addListener(updateSlick) // Attach listener function on state changes
+
+
+})
+
+// $shopBy.on('breakpoint', function(event, slick, breakpoint) {
+//     console.log('breakpoint' + breakpoint);
+// })
+
+
